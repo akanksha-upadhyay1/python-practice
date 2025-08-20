8acee7a1e184bb4b074d97463b2bb1b93a033b62
akanksha-upadhyay1 <akanksha2445@gmail.com>
2025-08-20 12:44:46 +0530

    strings

---
+++ chapter3/escape_seq.py
@@ -0,0 +1,3 @@
+
+a = "harry is a \\good boy\nbut \"not\" a bad \nboy"
+print(a)
---
+++ chapter3/negative_slicing.py
@@ -0,0 +1,5 @@
+name = "harry"
+print(name[0:4])
+print(name[-4:-1]) 
+print(name[ :5]) # is as same as print(name[0:4])
+print(name[0: ]) # is as same as print(name[0:5])
