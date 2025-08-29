e8630e68507caad2a143ce95b9268bd6ab8b34bf
akanksha-upadhyay1 <akanksha2445@gmail.com>
2025-08-29 18:08:20 +0530

    break and continuation chapter

---
+++ chapter7/for.loop.iterate.py
@@ -0,0 +1,14 @@
+#For loop with list
+# l = [1, 234, 4, 76, 89]
+# for i in l:
+#     print(i)
+
+#For loop with tuple
+# t = (2, 34, 56, 78, 54)
+# for i in t:
+#     print(i)
+
+#For loop with strings
+s = "Harry"
+for i in s:
+    print(i)
