c07e1f0cdd2844ae97e7e19afc34c92b45e692bc
Akanksha Upadhyay <akanksha2445@gmail.com>
2025-09-04 15:26:00 +0530

    append, file, with, write mode

---
+++ chapter9/append.py
@@ -0,0 +1,5 @@
+st = "you are my favourite person"
+f = open("myfile.txt", "a")
+f.write(st)
+f.close()
+print(st )
---
+++ chapter9/file.py
@@ -0,0 +1,9 @@
+'''
+ a = "a very long string with emails"
+  
+'''
+f = open("file.txt")
+data = f.read()
+print(data)
+f.close()
+ 
